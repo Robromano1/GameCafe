edf4c0c625485ec5b4f7c3b1e3f7301b582cae36
Robert <Robromano2@gmail.com>
2020-02-11 16:25:58 -0500

    Add session form

---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            username: "",
+            password: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    update(field) {
+        return e => this.setState({ [field]: e.currentTarget.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state)
+        this.props.processForm(user);
+    }
+
+    renderErrors() {
+        return (
+            <ul>
+                {this.props.errors.map(error => (
+                    <li key={`error-${error.id}`}>
+                        {error}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
+    render() {
+        const { formType, navLink } = this.props;
+        return(
+            <div className="login-form-container">
+                <form onSubmit={this.handleSubmit}>
+                    Please {formType} or {navLink}
+                    {this.renderErrors()}
+                    <br/>
+                    <div className="login-form">
+                        <label>Username:
+                            <input type="text" 
+                            value={this.state.username}
+                            onChange={this.update('username')}/>
+                        </label>
+                        <label>Password:
+                            <input type="password"
+                                value={this.state.password}
+                                onChange={this.update('password')} />
+                        </label>
+                        <input type="submit" value={formType}/>
+                        <br/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SessionForm;

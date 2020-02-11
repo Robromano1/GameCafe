a78fb903b99b711ad9bdbe537425ea6961d1dc9c
Robert <Robromano2@gmail.com>
2020-02-11 17:17:23 -0500

    Add auth routes

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,42 @@
+class Api::UsersController < ApplicationController
+    def index
+        @user = User.all
+        render :index
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show 
+        else
+            render json: @user.errors.full_messages, status 422
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def update
+        @user = selected_user
+        if @user 
+            @user.update_attributes(user_params)
+            render :show
+        elsif !@user 
+            render ["User does not exist"]
+        else
+            render @user.errors.full_messages, status 401
+        end
+    end
+
+    private
+    def user_params 
+        params.require(:user).permit(:username, :password)
+    end
+
+    def selected_user
+        User.find(params[:id])
+    end
+end

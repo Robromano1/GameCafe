d5e3e178c2323b46b4c02dc446da01e1fd255e85
Robert <Robromano2@gmail.com>
2020-02-11 09:34:07 -0500

    Add user auth/migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,41 @@
 class User < ApplicationRecord
-    #FIGVAPER
-    # validates: 
+  
+    validates :username, :email, :password_digest, 
+              :session_token, :image_url, presence: true
+    validates :username, :email, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
 
-    # attr_reader :password
+    attr_reader :password
+    after_initialize :ensure_session_token
 
-    # def self.find_by_credentials(username, password)
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
 
-    # end
+    end
+    
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
 
-    # def password=(password)
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
 
-    # end
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+    
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
 
-    # def is_password?(password)
-
-    # end
-
-    # def generate_session_token
-
-    # end
-
-    # def reset_session_token!
-
-    # end
-
-    # private
-    # def ensure_session_token
-
-    # end
+    private
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
 end
---
+++ db/schema.rb
@@ -0,0 +1,73 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_10_195730) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.integer "server_id", null: false
+    t.string "channel_name", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["server_id"], name: "index_channels_on_server_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "user_id", null: false
+    t.integer "server_id", null: false
+    t.integer "channel_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["server_id"], name: "index_messages_on_server_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "server_users", force: :cascade do |t|
+    t.integer "server_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["server_id"], name: "index_server_users_on_server_id"
+    t.index ["user_id"], name: "index_server_users_on_user_id"
+  end
+
+  create_table "servers", force: :cascade do |t|
+    t.string "server_name", null: false
+    t.integer "admin_id", null: false
+    t.string "description", null: false
+    t.string "server_image", null: false
+    t.boolean "private", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_servers_on_admin_id"
+    t.index ["server_name"], name: "index_servers_on_server_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.string "image_url", null: false
+    t.integer "server_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+end
